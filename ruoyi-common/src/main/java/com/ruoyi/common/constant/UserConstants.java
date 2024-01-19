6d99eecad059bd6144d9a801ce209c017d2ac2dc
jinmu1 <1612429531@qq.com>
2024-01-19 15:08:33 +0800

    最新修改1.18.1

--- ruoyi-common/src/main/java/com/ruoyi/common/constant/UserConstants.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/constant/UserConstants.java
@@ -44,14 +44,14 @@ public class UserConstants
      * 用户名长度限制
      */
     public static final int USERNAME_MIN_LENGTH = 2;
-    public static final int USERNAME_MAX_LENGTH = 20;
+    public static final int USERNAME_MAX_LENGTH = 50;
 
     /**
      * 密码长度限制
      */
     public static final int PASSWORD_MIN_LENGTH = 5;
     public static final int PASSWORD_MAX_LENGTH = 20;
-
+    public final static String USER_NAME_NOT_UNIQUE = "1";
     /**
      * 用户类型
      */
--- ruoyi-framework/src/main/java/com/ruoyi/framework/shiro/service/SysRegisterService.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/shiro/service/SysRegisterService.java
@@ -80,4 +80,46 @@ public class SysRegisterService
         }
         return msg;
     }
+
+
+    public String add(SysUser user) {
+        String msg = "", loginName = user.getLoginName(), password = user.getPassword();
+         if (org.springframework.util.StringUtils.isEmpty(loginName))
+        {
+            msg = "用户名不能为空";
+        }
+        else if (org.springframework.util.StringUtils.isEmpty(password))
+        {
+            msg = "用户密码不能为空";
+        }
+        else if (password.length() < UserConstants.PASSWORD_MIN_LENGTH
+                || password.length() > UserConstants.PASSWORD_MAX_LENGTH)
+        {
+            msg = "密码长度必须在5到20个字符之间";
+        }
+
+        else if (UserConstants.USER_NAME_NOT_UNIQUE.equals(userService.checkLoginNameUnique(user)))
+        {
+            msg = "保存用户'" + loginName + "'失败，注册账号已存在";
+        }
+        else
+        {
+            user.setPwdUpdateDate(DateUtils.getNowDate());
+            user.setUserName(loginName);
+            user.setSalt(ShiroUtils.randomSalt());
+            user.setPassword(passwordService.encryptPassword(user.getLoginName(), user.getPassword(), user.getSalt()));
+            user.setUserType(UserConstants.REGISTER_USER_TYPE);
+            boolean regFlag = userService.registerUser(user);
+            if (!regFlag)
+            {
+                msg = "注册失败,请联系系统管理人员";
+            }
+            else
+            {
+                AsyncManager.me().execute(AsyncFactory.recordLogininfor(loginName, Constants.REGISTER, MessageUtils.message("user.register.success")));
+                msg = "success";
+            }
+        }
+        return msg;
+    }
 }
