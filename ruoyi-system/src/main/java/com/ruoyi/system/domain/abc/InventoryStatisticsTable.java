26348b348a097ccf47f3c407f36e575c085ebb64
jinmu1 <1612429531@qq.com>
2024-01-26 10:54:30 +0800

    ABC类名的修改 :1.去掉了get和set方法，           2.修改类类名称

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/abc/InventoryStatisticsTable.java
@@ -0,0 +1,49 @@
+package com.ruoyi.system.domain.abc;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ruoyi.common.annotation.Excel;
+import lombok.Getter;
+import lombok.Setter;
+
+public class InventoryStatisticsTable {
+    @Excel(name = "物料编码")
+    @JsonProperty("物料编码")
+    @Getter
+    @Setter
+    private String materialCode;
+
+    @Getter
+    @Setter
+    @JsonProperty("平均库存")
+    @Excel(name = "平均库存（件/月）")
+    private int averageInventory;
+
+    @Getter
+    @Setter
+    @JsonProperty("当月出库总量")
+    @Excel(name = "当月出库总量（件）")
+    private double monthlyTotalOutbound;
+
+    @Getter
+    @Setter
+    @JsonProperty("库存周转次数")
+    @Excel(name = "库存周转次数")
+    private int inventoryTurnover;
+
+    @Getter
+    @Setter
+    @JsonProperty("库存周转天数")
+    @Excel(name = "库存周转天数")
+    private int inventoryTurnoverDays;
+
+
+    @Getter
+    @Setter
+    @JsonProperty("销售单价")
+    @Excel(name = "销售单价（元/件）")
+    private double sellingPrice;
+
+
+
+
+}
---
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/abc/OutboundTable.java
@@ -0,0 +1,63 @@
+package com.ruoyi.system.domain.abc;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ruoyi.common.annotation.Excel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+public class OutboundTable {
+    @Getter
+    @Setter
+    @JsonProperty("出库日期")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd")
+    @Excel(name = "出库日期")
+    private Date deliveryDate;
+
+    @Getter
+    @Setter
+    @JsonProperty("订单编号")
+    @Excel(name = "订单编号")
+    private long orderNumber;
+
+    @Getter
+    @Setter
+    @JsonProperty("物料编号")
+    @Excel(name = "物料编号")
+    private String materialNumber;
+
+    @Getter
+    @Setter
+    @JsonProperty("物料名称")
+    @Excel(name = "物料名称")
+    private String materialName;
+
+    @Getter
+    @Setter
+    @JsonProperty("出货数量")
+    @Excel(name = "出货数量")
+    private double shippedQuantity;
+
+    @Getter
+    @Setter
+    @JsonProperty("出货单位")
+    @Excel(name = "出货单位")
+    private String shipmentUnit;
+
+    @Getter
+    @Setter
+    @JsonProperty("销售单价")
+    @Excel(name = "销售单价（元/件）")
+    private double unitPrice;
+
+    @Getter
+    @Setter
+    @JsonProperty("托盘装件数")
+    @Excel(name = "托盘装件数")
+    private double palletizedItemCount;
+
+
+
+}
