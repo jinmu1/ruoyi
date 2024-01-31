553483be9ff19dc427edef9851832d2e5b8fda35
jinmu1 <1612429531@qq.com>
2024-01-31 15:39:13 +0800

    将导入的方法重构为公用方法，在页面调用

--- ruoyi-admin/src/main/resources/static/js/baidie/upload.js
+++ ruoyi-admin/src/main/resources/static/js/baidie/upload.js
@@ -1,66 +1,69 @@
-
-function upload(url){
+/***
+ * Excel导入方法
+ * @param url：导入的后端请求路径
+ */
+function importData(url) {
     var formId, width, height;
     var currentId = $.common.isEmpty(formId) ? 'importTpl' : formId;
     var _width = $.common.isEmpty(width) ? "400" : width;
     var _height = $.common.isEmpty(height) ? "230" : height;
+
     window.layer.open({
         type: 1,
         area: [_width + 'px', _height + 'px'],
         fix: false,
-        //不固定
         maxmin: true,
         shade: 0.3,
-        title: '导入'  + '数据',
+        title: '导入数据',
         content: $('#' + currentId).html(),
         btn: ['<i class="fa fa-check"></i> 导入', '<i class="fa fa-remove"></i> 取消'],
-        // 弹层外区域关闭
         shadeClose: true,
-        btn1: function(index, layero){
+        btn1: function(index, layero) {
             var file = layero.find('#file').val();
-            if (file == '' || ( !$.common.endWith(file, '.xlsx') )){
-                window.layer.msgWarning("请选择后缀为“xlsx”的文件。");
+            if (file == '' || (!file.endsWith('.xls') && !file.endsWith('.xlsx') && !file.endsWith('.csv'))) {
+                window.layer.msgWarning("请选择后缀为 “xls”、“csv”或“xlsx”的文件。");
                 return false;
             }
-            var index = window.layer.load(2, {shade: false});
+
             var formData = new FormData(layero.find('form')[0]);
+            var loadingIndex = window.layer.load(2, {shade: false});
             $.ajax({
-                url: ctx+ url, //加上ctx后，从百碟客户端可访问我们线上路径
+                url: ctx+url,
                 data: formData,
                 cache: false,
-                async: false,
                 contentType: false,
                 processData: false,
                 type: 'POST',
-                success: function (result) {
+                success: function(result) {
+
                     if (result.code == web_status.SUCCESS) {
-                        window.layer.close(index);
-                        window.layer.closeAll();
-                        $.modal.alertSuccess(result.msg);
+                        popUpWindow($.modal.icon("success"), result.msg);
                         $.table.refresh();
-                         return result;
-                    } else if (result.code == web_status.WARNING) {
-                        window.layer.close(index);
-                        window.layer.enable();
-                        $.modal.alertWarning(result.msg)//将失败信息返回，提示框为警告样式
-                        return ;
+                        handleImportResult(url,result);//页面的对返回数据的处理方法
                     } else {
-                        window.layer.close(index);
-                        window.layer.enable();
-                        $.modal.alertWarning(result.msg)
-                        return ;
+                        popUpWindow($.modal.icon("warning"), result.msg);
                     }
                 },
-                complete: function () {
+                complete: function() {
                     layero.find('#file').val('');
+                    window.layer.close(loadingIndex);
                 }
             });
         }
     });
 }
-function isValueEmpty(value) {
-    return value === undefined || value === null ||
-        (typeof value === 'string' && value.trim() === '') ||
-        (Array.isArray(value) && value.length === 0) ||
-        (typeof value === 'object' && Object.keys(value).length === 0 && value.constructor === Object);
+
+/***
+ * 弹出返回的结果提示信息
+ * @param icon
+ * @param msg
+ */
+function popUpWindow(icon, msg) {
+    window.layer.closeAll();
+    window.layer.alert(msg, {
+        icon: icon,
+        title: "系统提示",
+        btn: ['确认'],
+        btnclass: ['btn btn-primary'],
+    });
 }
