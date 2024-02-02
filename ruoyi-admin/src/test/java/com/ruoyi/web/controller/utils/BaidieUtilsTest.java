f792b1970e79662ca9d5e048230b5de288a19171
jinmu1 <1612429531@qq.com>
2024-02-02 16:11:53 +0800

    重新按照修改建议修改， 1.git rebase 2.修改了private 3.新增了Test方法

--- ruoyi-admin/src/test/java/com/ruoyi/web/controller/utils/BaidieUtilsTest.java
+++ ruoyi-admin/src/test/java/com/ruoyi/web/controller/utils/BaidieUtilsTest.java
@@ -1,5 +1,7 @@
 package com.ruoyi.web.controller.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ruoyi.common.json.JSONObject;
 import com.ruoyi.web.controller.system.ABCAnalyseController;
 import org.junit.Test;
 import org.springframework.http.MediaType;
@@ -7,6 +9,8 @@ import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -50,5 +54,27 @@ public class BaidieUtilsTest {
                 BaidieUtils.parseFromExcelFile(mockMultipartFile, ABCAnalyseController.Data1Entry.class);
 
         assertEquals(result.size(), 11);
+    }
+
+
+    // 测试将List对像转换为对应的Json对象.
+    @Test
+    public void testGenerateResponseJson() throws JsonProcessingException {
+        // 准备测试数据
+        HashMap<String, List<?>> jsonKeyToValue = new HashMap<>();
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        List<String> list2 = new ArrayList<>();
+        list2.add("a");
+        list2.add("b");
+        jsonKeyToValue.put("key1", list1);
+        jsonKeyToValue.put("key2", list2);
+
+        // 调用方法生成 JSON
+        JSONObject jsonObject = BaidieUtils.generateResponseJson(jsonKeyToValue);
+
+        // 验证生成的 JSON 是否符合预期
+        assertEquals("{\"key1\":\"[1,2]\",\"key2\":\"[a,b]\"}", jsonObject.toString());
     }
 }
