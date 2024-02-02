d44c35b7a28afc355ee6b6bb309887c43b658f6c
jinmu1 <1612429531@qq.com>
2024-02-02 16:25:31 +0800

    重新按照修改建议修改， 1.git rebase

--- ruoyi-admin/src/main/resources/static/js/baidie/upload.js
+++ ruoyi-admin/src/main/resources/static/js/baidie/upload.js
@@ -1,66 +1,98 @@
 
-function upload(url){
-    var formId, width, height;
-    var currentId = $.common.isEmpty(formId) ? 'importTpl' : formId;
-    var _width = $.common.isEmpty(width) ? "400" : width;
-    var _height = $.common.isEmpty(height) ? "230" : height;
+
+/***
+ * Excel导入方法
+ * @param url：导入的后端请求路径
+ */
+function importData(url,updateGroup) {
+    var _width =  "400";
+    var _height = "230";
+    //这里调用的是layer.js : https://layui.itze.cn/doc/modules/layer.html
     window.layer.open({
-        type: 1,
-        area: [_width + 'px', _height + 'px'],
-        fix: false,
-        //不固定
-        maxmin: true,
-        shade: 0.3,
-        title: '导入'  + '数据',
-        content: $('#' + currentId).html(),
-        btn: ['<i class="fa fa-check"></i> 导入', '<i class="fa fa-remove"></i> 取消'],
-        // 弹层外区域关闭
-        shadeClose: true,
-        btn1: function(index, layero){
-            var file = layero.find('#file').val();
-            if (file == '' || ( !$.common.endWith(file, '.xlsx') )){
-                window.layer.msgWarning("请选择后缀为“xlsx”的文件。");
-                return false;
-            }
-            var index = window.layer.load(2, {shade: false});
-            var formData = new FormData(layero.find('form')[0]);
-            $.ajax({
-                url: ctx+ url, //加上ctx后，从百碟客户端可访问我们线上路径
-                data: formData,
-                cache: false,
-                async: false,
-                contentType: false,
-                processData: false,
-                type: 'POST',
-                success: function (result) {
-                    if (result.code == web_status.SUCCESS) {
-                        window.layer.close(index);
-                        window.layer.closeAll();
-                        $.modal.alertSuccess(result.msg);
-                        $.table.refresh();
-                         return result;
-                    } else if (result.code == web_status.WARNING) {
-                        window.layer.close(index);
-                        window.layer.enable();
-                        $.modal.alertWarning(result.msg)//将失败信息返回，提示框为警告样式
-                        return ;
-                    } else {
-                        window.layer.close(index);
-                        window.layer.enable();
-                        $.modal.alertWarning(result.msg)
-                        return ;
-                    }
-                },
-                complete: function () {
-                    layero.find('#file').val('');
-                }
-            });
+        type: 1, // 弹出层类型，这里是普通的页面层
+        area: [_width + 'px', _height + 'px'], // 弹出层的尺寸，使用_width和_height变量来指定宽度和高度
+        fix: false, // 是否固定在可视区域
+        maxmin: true, // 是否显示最大化和最小化按钮
+        shade: 0.3, // 遮罩层的透明度
+        title: '导入数据', // 弹出层的标题
+        content: generateImportFormTemplate(), // 弹出层的内容，这里使用jQuery获取指定ID的元素的HTML内容
+        btn: ['<i class="fa fa-check"></i> 导入', '<i class="fa fa-remove"></i> 取消'], // 弹出层的按钮组
+        shadeClose: true, // 是否点击遮罩层关闭弹出层
+        btn1: function(index, layero) {
+            uploadFileToBackend(index, layero, url, updateGroup);//调用ajax方法和处理返回的数据
         }
     });
 }
-function isValueEmpty(value) {
-    return value === undefined || value === null ||
-        (typeof value === 'string' && value.trim() === '') ||
-        (Array.isArray(value) && value.length === 0) ||
-        (typeof value === 'object' && Object.keys(value).length === 0 && value.constructor === Object);
+
+/***
+ * 弹出返回的结果提示信息
+ * @param icon
+ * @param msg
+ */
+function popUpWindow(icon, msg) {
+    window.layer.closeAll();
+    window.layer.alert(msg, {
+        icon: icon,
+        title: "系统提示",
+        btn: ['确认'],
+        btnclass: ['btn btn-primary'],
+    });
+}
+
+/***
+ * 将导入的html模版作为一个对象
+ * @returns {HTMLScriptElement}
+ */
+function generateImportFormTemplate() {
+    var htmlContent = '\
+        <form enctype="multipart/form-data" class="mt20 mb10">\
+            <div class="col-xs-offset-1">\
+                <input type="file" id="file" name="file"/>\
+                <div class="mt10 pt5">\
+                </div>\
+                <font color="red" class="pull-left mt10">\
+                    提示：仅允许导入“xlsx”格式文件！\
+                </font>\
+            </div>\
+        </form>';
+    return htmlContent;
+}
+
+/***
+ * 导入数据和处理导出的数据
+ * @param index
+ * @param layero
+ * @param updateGroup
+ * @returns {boolean}
+ */
+function uploadFileToBackend(index, layero, url, updateGroup) {
+    var file = layero.find('#file').val();
+    if (file == '' || (!file.endsWith('.xlsx'))) {
+        window.layer.msgWarning("请选择后缀为“xlsx”的文件。");
+        return false;
+    }
+    var formData = new FormData(layero.find('form')[0]);
+    var loadingIndex = window.layer.load(2, {shade: false});
+    $.ajax({
+        url: ctx+url,
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        type: 'POST',
+        success: function(result) {
+
+            if (result.code == web_status.SUCCESS) {
+                popUpWindow($.modal.icon("success"), result.msg);
+                $.table.refresh();
+                updateGroup(result);//页面的对返回数据的处理方法
+            } else {
+                popUpWindow($.modal.icon("warning"), result.msg);
+            }
+        },
+        complete: function() {
+            layero.find('#file').val('');
+            window.layer.close(loadingIndex);
+        }
+    });
 }
