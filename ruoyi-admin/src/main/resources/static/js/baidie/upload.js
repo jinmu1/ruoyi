a34f52861e0f772dbc5e459ffc80c99484bab82c
jinmu1 <1612429531@qq.com>
2024-02-03 15:01:14 +0800

    upload，导入方法的重构 (#21)
    * 增加了js文件
    
    * 将导入的方法重构为公用方法，在页面调用
    
    * 重新按照修改建议修改，
    1.增加了描述
    2.将导出方法的处理了方式修改
    3.修改了部分描述。
    
    * 重新按照修改建议修改，
    1.git rebase
    
    * 重新按照修改建议修改，
    1.git rebase
    
    * 修改了部分方法
    
    * 加注释

---
+++ ruoyi-admin/src/main/resources/static/js/baidie/upload.js
@@ -0,0 +1,97 @@
+
+
+/***
+ * Excel导入方法
+ * @param url：导入的后端请求路径
+ */
+function importData(url, refresh_data_callback) {
+    const  _width =  "400";
+    const _height = "230";
+    //这里调用的是layer.js : https://layui.itze.cn/doc/modules/layer.html
+    window.layer.open({
+        type: 1, // 弹出层类型，这里是普通的页面层
+        area: [_width + 'px', _height + 'px'], // 弹出层的尺寸，使用_width和_height变量来指定宽度和高度
+        fix: false, // 是否固定在可视区域
+        maxmin: true, // 是否显示最大化和最小化按钮
+        shade: 0.3, // 遮罩层的透明度
+        title: '导入数据', // 弹出层的标题
+        content: generateImportFormTemplate(), // 弹出层的内容，这里使用jQuery获取指定ID的元素的HTML内容
+        btn: ['<i class="fa fa-check"></i> 导入', '<i class="fa fa-remove"></i> 取消'], // 弹出层的按钮组
+        shadeClose: true, // 是否点击遮罩层关闭弹出层
+        btn1: function(index, layero) {
+            uploadFileToBackend(index, layero, url, refresh_data_callback);//调用ajax方法和处理返回的数据
+        }
+    });
+}
+
+/***
+ * 弹出返回的结果提示信息
+ * @param icon 提示符号
+ * @param msg  返回的提示信息
+ */
+function popUpWindow(icon, msg) {
+    window.layer.closeAll();
+    window.layer.alert(msg, {
+        icon: icon,
+        title: "系统提示",
+        btn: ['确认'],
+        btnclass: ['btn btn-primary'],
+    });
+}
+
+/***
+ * 将导入的html模版作为一个对象
+ * @returns {HTMLScriptElement}
+ */
+function generateImportFormTemplate() {
+    var htmlContent = '\
+        <form enctype="multipart/form-data" class="mt20 mb10">\
+            <div class="col-xs-offset-1">\
+                <input type="file" id="file" name="file"/>\
+                <div class="mt10 pt5">\
+                </div>\
+                <font color="red" class="pull-left mt10">\
+                    提示：仅允许导入“xlsx”格式文件！\
+                </font>\
+            </div>\
+        </form>';
+    return htmlContent;
+}
+
+/***
+ * 导入数据和处理导出的数据
+ * @param index 导入文件ID
+ * @param layero 弹出框
+ * @param refresh_data_callback  返回值参数
+ * @returns {boolean}
+ */
+function uploadFileToBackend(index, layero, url, refresh_data_callback) {
+    var file = layero.find('#file').val();
+    if (file == '' || !file.endsWith('.xlsx')) {
+        popUpWindow($.modal.icon("warning"),"请选择后缀为“xlsx”的文件。");
+        return false;
+    }
+    var formData = new FormData(layero.find('form')[0]);
+    var loadingIndex = window.layer.load(2, {shade: false});// 创建一个加载动画，loadingIndex用于保存加载动画的索引 0: 默认的加载动画样式，即圆圈旋转。1: 简单的加载动画样式，即水平方向的滚动条。2: 进度条加载动画样式，显示一个进度条。
+    $.ajax({
+        url: ctx+url,
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        type: 'POST',
+        success: function(result) {
+            if (result.code == web_status.SUCCESS) {
+                popUpWindow($.modal.icon("success"), result.msg);
+                $.table.refresh();
+                refresh_data_callback(result);//页面的对返回数据的处理方法
+            } else {
+                popUpWindow($.modal.icon("warning"), result.msg);
+            }
+        },
+        complete: function() {
+            layero.find('#file').val('');
+            window.layer.close(loadingIndex);
+        }
+    });
+}
