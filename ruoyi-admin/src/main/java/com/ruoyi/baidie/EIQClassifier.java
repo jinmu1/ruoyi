5a8006a54f5cb09479974786aea75b0a3e658f91
jinmu1 <1612429531@qq.com>
2024-02-05 22:38:21 +0800

    1.将EIQ的E分析、N分析、Q分析的基本计算方法写在EIQClassifier 2.增加了测试类测试方法

---
+++ ruoyi-admin/src/main/java/com/ruoyi/baidie/EIQClassifier.java
@@ -0,0 +1,75 @@
+package com.ruoyi.baidie;
+
+
+import com.ruoyi.common.utils.DateUtils;
+import com.ruoyi.data.eiq.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+/****
+ * EIQ 处理类：将EIQ分析的各项统计指标进行转换
+ */
+public class EIQClassifier {
+
+    /***
+     * 将导入的数据转换为EIQ单项要素分析数据统计表(data2)
+     * 1.首先对数据按照日期分组
+     * 2.按照每天统计
+     * @param eiqBasicTableList 初始化的数据表
+     * @return 返回的是包含EIQ单项要素分析的数据（data2）
+     */
+    public static List<EIQAnalysisTable> getEIQAnalysisTable(List<EIQBasicTable> eiqBasicTableList){
+        //step1: 将日期标准化，防止同一天出现两个值
+        List<EIQBasicTable> standardizedList = eiqBasicTableList.stream()
+                .map(basicTable -> {
+                    EIQBasicTable newBasicTable = new EIQBasicTable();
+                    newBasicTable.setDeliveryDate(normalizeDate(basicTable.getDeliveryDate()));
+                    return newBasicTable;
+                })
+                .collect(Collectors.toList());
+        //step2: 首先将数据按出库日期重组
+        final  Map<Date, List<EIQBasicTable>> dataGroupByDeliveryDate =
+                standardizedList.stream().collect(
+                        Collectors.groupingBy(EIQBasicTable::getDeliveryDate));
+        //step3: 统计每个出库日期的数据，然后升序排序
+        final List<EIQAnalysisTable> eIQAnalysisTableSortedByDeliveryDate =
+                dataGroupByDeliveryDate.entrySet().stream()
+                        .map(entry -> calculateOutboundInfo(entry.getKey(), entry.getValue()))
+                        .sorted(Comparator.comparing(EIQAnalysisTable::getDate))
+                        .collect(Collectors.toList());
+        return eIQAnalysisTableSortedByDeliveryDate;
+
+    }
+    /***
+     * EIQ分析--以日期作为统计数据的分类
+     * @param key  日期
+     * @param eiqBasicTablesGroupByDate 导入的基本数据类
+     * @return 返回处理过的EIQ分析的值
+     */
+    private static EIQAnalysisTable calculateOutboundInfo(Date key, List<EIQBasicTable> eiqBasicTablesGroupByDate) {
+        EIQAnalysisTable eiqAnalysisTable = new EIQAnalysisTable();
+        Map<String, List<EIQBasicTable>> collect = eiqBasicTablesGroupByDate.stream()
+                .collect(Collectors.groupingBy(EIQBasicTable::getOrderNumber));
+        eiqAnalysisTable.setDate(key);
+        eiqAnalysisTable.setEAnalysis(collect.size());
+        eiqAnalysisTable.setNAnalysis(eiqBasicTablesGroupByDate.size());
+        double sum = 0;
+        // 遍历当前日期的 EIQBasicTables 列表，累加出库量
+        for (EIQBasicTable dataItem : eiqBasicTablesGroupByDate) {
+            sum += dataItem.getDeliveryQuantity();
+        }
+        eiqAnalysisTable.setQAnalysis(sum);
+        return eiqAnalysisTable;
+    }
+    /**
+     * 将日期格式标准化 比如将2023-2-5 14:46:12 转换为 2023-2-5 00:00:00
+     * @param deliveryDate
+     * @return
+     */
+    private static Date normalizeDate(Date deliveryDate){
+        return DateUtils.truncate(deliveryDate, Calendar.DATE);
+    }
+
+}
