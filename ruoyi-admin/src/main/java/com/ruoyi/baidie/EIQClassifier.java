bb6b45a5941e3150da2e24ead04c7c769411fdd9
jinmu1 <1612429531@qq.com>
2024-02-08 13:33:01 +0800

    EN方法 (#43)
    * 1.将EIQ的EN分析的基本计算方法写在EIQClassifier
    2.增加了测试类测试方法
    
    * 1.将EIQ的EN分析的基本计算方法写在EIQClassifier
    2.增加了测试类测试方法

--- ruoyi-admin/src/main/java/com/ruoyi/baidie/EIQClassifier.java
+++ ruoyi-admin/src/main/java/com/ruoyi/baidie/EIQClassifier.java
@@ -6,6 +6,7 @@ import com.ruoyi.data.eiq.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 /****
@@ -85,4 +86,44 @@ public class EIQClassifier {
         return DateUtils.truncate(deliveryDate, Calendar.DATE);
     }
 
+    /**
+     * 将导入的数据转换为EN综合分析数据统计表(data3)
+     * 1.首先按照订单编码分组
+     * 2.按照订单行数统计赋值
+     *
+     * @param eiqBasicTableList 导入的基本数据表
+     * @return EN综合分析数据统计表
+     */
+    public static List<ENAnalysisTable> getENAnalysisTable(List<EIQBasicTable> eiqBasicTableList) {
+        // Step 1: 首先将数据按订单编码重组
+        final Map<String, List<EIQBasicTable>> dataGroupByOrderNumber =
+                eiqBasicTableList.stream().collect(Collectors.groupingBy(EIQBasicTable::getOrderNumber));
+
+        // Step 2: 统计每个订单编码的行数，然后降序排序
+        final List<ENAnalysisTable> enAnalysisTableSortedByOrderLineCount =
+                dataGroupByOrderNumber.entrySet().stream()
+                        .map(entry -> calculateOrderInfo(entry.getKey(), entry.getValue()))
+                        .sorted(Comparator.comparing(ENAnalysisTable::getOrderLineCount, Comparator.reverseOrder()))
+                        .collect(Collectors.toList());
+
+        // Step 3: 设置一个自增的序号
+        IntStream.range(0, enAnalysisTableSortedByOrderLineCount.size())
+                .forEach(i -> enAnalysisTableSortedByOrderLineCount.get(i).setCumulativeItemNumber(i + 1));
+
+        return enAnalysisTableSortedByOrderLineCount;
+    }
+
+    /**
+     * EN分析--以订单编码作为统计数据的分类
+     *
+     * @param key 订单编码
+     * @param eiqBasicTables 导入的基本数据类
+     * @return 返回处理过的EN分析的值
+     */
+    private static ENAnalysisTable calculateOrderInfo(String key, List<EIQBasicTable> eiqBasicTables) {
+        ENAnalysisTable enAnalysisTable = new ENAnalysisTable();
+        enAnalysisTable.setOrderNumber(key);
+        enAnalysisTable.setOrderLineCount(eiqBasicTables.size());
+        return enAnalysisTable;
+    }
 }
