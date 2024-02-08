0b4dbacf65bf8ecf8744869cf5091ba81ae9ab9d
jinmu1 <1612429531@qq.com>
2024-02-08 15:53:32 +0800

    1.将EIQ的EQ分析的基本计算方法写在EIQClassifier 2.增加了测试类测试方法EQAnalysisTableTest

--- ruoyi-admin/src/test/java/com/ruoyi/baidie/EIQAnalysisTableTest.java
+++ ruoyi-admin/src/test/java/com/ruoyi/baidie/EIQAnalysisTableTest.java
@@ -25,23 +25,24 @@ public class EIQAnalysisTableTest {
     private int expectedTotalNumOrders;
     private List<Double> testEQAnalysis;
     private List<String> testEQOrderNumber;
+
     @Before
     public void setUp() {
         // Set up some test data
-        EIQBasicTable obj1 = createBasicTableInfoWithDate(createDate(2024, 2, 5, 3,8,2), "1234", "M1", "Material 1", 100.0, "kg", 10.0, 2.0, 1.0, 0.5);
-        EIQBasicTable obj2 = createBasicTableInfoWithDate(createDate(2024, 2, 5, 3,6,2), "5678", "M2", "Material 2", 150.0, "lbs", 15.0, 3.0, 1.5, 0.7);
-        EIQBasicTable obj3 = createBasicTableInfoWithDate(createDate(2024, 2, 5, 3,4,22), "91011", "M3", "Material 3", 200.0, "tons", 20.0, 4.0, 2.0, 1.0);
-        EIQBasicTable obj4 = createBasicTableInfoWithDate(createDate(2024, 2, 5, 3,9,23), "121314", "M4", "Material 4", 250.0, "grams", 25.0, 5.0, 2.5, 1.2);
-        EIQBasicTable obj5 = createBasicTableInfoWithDate(createDate(2024, 2, 4, 3,5,21), "151617", "M5", "Material 5", 300.0, "kg", 30.0, 6.0, 3.0, 1.5);
-        EIQBasicTable obj6 = createBasicTableInfoWithDate(createDate(2024, 2, 4, 3,5,2), "181920", "M6", "Material 6", 350.0, "lbs", 35.0, 7.0, 3.5, 1.7);
-        EIQBasicTable obj7 = createBasicTableInfoWithDate(createDate(2024, 2, 3, 3,5,2), "212223", "M7", "Material 7", 400.0, "tons", 40.0, 8.0, 4.0, 2.0);
-        EIQBasicTable obj8 = createBasicTableInfoWithDate(createDate(2024, 2, 3, 3,5,2), "212223", "M9", "Material 9", 500.0, "kg", 50.0, 10.0, 5.0, 2.5);
-        EIQBasicTable obj9 = createBasicTableInfoWithDate(createDate(2024, 2, 2, 3,5,2), "303132", "M10", "Material 10", 550.0, "lbs", 55.0, 11.0, 5.5, 2.7);
+        EIQBasicTable obj1 = createBasicTableInfoWithDate(createDate(2024, 2, 5, 3, 8, 2), "1234", "M1", "Material 1", 100.0, "kg", 10.0, 2.0, 1.0, 0.5);
+        EIQBasicTable obj2 = createBasicTableInfoWithDate(createDate(2024, 2, 5, 3, 6, 2), "5678", "M2", "Material 2", 150.0, "lbs", 15.0, 3.0, 1.5, 0.7);
+        EIQBasicTable obj3 = createBasicTableInfoWithDate(createDate(2024, 2, 5, 3, 4, 22), "91011", "M3", "Material 3", 200.0, "tons", 20.0, 4.0, 2.0, 1.0);
+        EIQBasicTable obj4 = createBasicTableInfoWithDate(createDate(2024, 2, 5, 3, 9, 23), "121314", "M4", "Material 4", 250.0, "grams", 25.0, 5.0, 2.5, 1.2);
+        EIQBasicTable obj5 = createBasicTableInfoWithDate(createDate(2024, 2, 4, 3, 5, 21), "151617", "M5", "Material 5", 300.0, "kg", 30.0, 6.0, 3.0, 1.5);
+        EIQBasicTable obj6 = createBasicTableInfoWithDate(createDate(2024, 2, 4, 3, 5, 2), "181920", "M6", "Material 6", 350.0, "lbs", 35.0, 7.0, 3.5, 1.7);
+        EIQBasicTable obj7 = createBasicTableInfoWithDate(createDate(2024, 2, 3, 3, 5, 2), "212223", "M7", "Material 7", 400.0, "tons", 40.0, 8.0, 4.0, 2.0);
+        EIQBasicTable obj8 = createBasicTableInfoWithDate(createDate(2024, 2, 3, 3, 5, 2), "212223", "M9", "Material 9", 500.0, "kg", 50.0, 10.0, 5.0, 2.5);
+        EIQBasicTable obj9 = createBasicTableInfoWithDate(createDate(2024, 2, 2, 3, 5, 2), "303132", "M10", "Material 10", 550.0, "lbs", 55.0, 11.0, 5.5, 2.7);
 
         testEIQBasicTable =
                 Arrays.asList(obj1, obj2, obj3, obj4, obj5, obj6, obj7, obj8, obj9);
         testSortByDates =
-                Arrays.asList(createDate(2024, 2, 2,0,0,0), createDate(2024, 2, 3,0,0,0), createDate(2024, 2, 4,0,0,0), createDate(2024, 2, 5,0,0,0));
+                Arrays.asList(createDate(2024, 2, 2, 0, 0, 0), createDate(2024, 2, 3, 0, 0, 0), createDate(2024, 2, 4, 0, 0, 0), createDate(2024, 2, 5, 0, 0, 0));
         testEAnalysis = Arrays.asList(1, 1, 2, 4);
         testNAnalysis = Arrays.asList(1, 2, 2, 4);
         testQAnalysis = Arrays.asList(550.0, 900.0, 650.0, 700.0);
@@ -102,6 +103,7 @@ public class EIQAnalysisTableTest {
         }
 
     }
+
     @Test
     public void getENAnalysisTableTest() {
         List<ENAnalysisTable> result = EIQClassifier.getENAnalysisTable(testEIQBasicTable);
@@ -131,6 +133,7 @@ public class EIQAnalysisTableTest {
         );
 
     }
+
     @Test
     public void getEQAnalysisTableTest() {
         List<EQAnalysisInfo> result = EIQClassifier.getEQAnalysisTable(testEIQBasicTable);
@@ -160,6 +163,7 @@ public class EIQAnalysisTableTest {
         );
 
     }
+
     //创建EIQ对象
     private EIQBasicTable createBasicTableInfoWithDate(Date deliveryDate,
                                                        String orderNumber,
