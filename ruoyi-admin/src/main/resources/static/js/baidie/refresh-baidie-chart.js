d4c7d4198fff9f2f172efd57d0a1788617ca2ae1
Fenglin Liao <fenglinlfl@gmail.com>
2024-02-17 19:04:36 +0800

    rewrite eiq page.

--- ruoyi-admin/src/main/resources/static/js/baidie/refresh-baidie-chart.js
+++ ruoyi-admin/src/main/resources/static/js/baidie/refresh-baidie-chart.js
@@ -101,3 +101,121 @@ function refreshBaidieChart(chartElementId,
     };
     chart.setOption(option);
 }
+
+function refreshChartWithMaxMin(chartElementId,
+                                data,
+                                xAttrName,
+                                yAttrName) {
+    var chart = echarts.init(document.getElementById(chartElementId), 'macarons');
+
+    const xAxisArray = data.map(entry => entry[xAttrName]);
+    const yAxisArray = data.map(entry => parseFloat(entry[yAttrName]));
+    const option= {
+        backgroundColor:'rgba(255, 255, 255, 0)',
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            show: true
+        },
+        dataZoom : {
+            show : true,
+            realtime : true
+        },
+        toolbox: {
+            show: true,
+            feature : {
+                mark : {show: true},
+                dataView : {show: true, readOnly: false},
+                magicType : {show: true, type: ['line', 'bar']},
+                restore : {show: true},
+                saveAsImage : {show: true}
+            }
+        },
+        xAxis:  {
+            type: 'category',
+            boundaryGap: false,
+            data: xAxisArray
+        },
+        yAxis: {
+            type: 'value',
+            axisLabel: {
+                formatter: '{value}'
+            },
+            splitLine:{
+                show: false
+            }
+        },
+        series: [
+            {
+                name: yAttrName,
+                type:'line',
+                data: yAxisArray,
+                itemStyle : {
+                    normal : {
+                        color:'#39c7aa'
+                    }
+                },
+                markPoint: {
+                    data: [
+                        {type: 'max', name: '最大值'},
+                        {type: 'min', name: '最小值'}
+                    ]
+                },
+                markLine: {
+                    data: [
+                        {type: 'average', name: '平均值'}
+                    ]
+                }
+
+            }
+        ]
+    };
+    chart.setOption(option);
+}
+
+function refreshBaidieBarChart(chartElementId,
+                               data,
+                               xAttrName,
+                               yAttrName) {
+    var chart = echarts.init(document.getElementById(chartElementId), 'macarons');
+
+    const xAxisArray = data.map(entry => entry[xAttrName]);
+    const yAxisArray = data.map(entry => parseFloat(entry[yAttrName]));
+    var option= {
+        backgroundColor:'rgba(255, 255, 255, 0)',
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            show: true
+        },
+        dataZoom : {
+            show : true,
+            realtime : true
+        },
+        xAxis: {
+            type: "category",
+            data: xAxisArray
+        },
+        yAxis: {
+            type: "value",
+            axisLabel: {
+                formatter: '{value}%'
+            },
+        },
+        series: [
+            {
+                name: yAttrName,
+                type: "bar",
+                data: yAxisArray,
+                itemStyle : {
+                    normal : {
+                        color:'#39c7aa'
+                    }
+                },
+            }
+        ]
+    };
+    chart.setOption(option);
+}
--- ruoyi-admin/src/main/resources/static/js/baidie/submit-select-group.js
+++ ruoyi-admin/src/main/resources/static/js/baidie/submit-select-group.js
@@ -1,45 +1,3 @@
-// 第一组下拉菜单的id和对应的正确选项值。
-const ABC_SELECT_GROUP_ONE_ANSWERS = new Map([
-    ["s1", "1"],
-    ["s2", "2"],
-    ["s3", "4"],
-    ["s4", "4"],
-    ["s5", "2"],
-    ["s6", "1"]
-]);
-
-const ABC_SELECT_GROUP_TWO_ANSWERS = new Map([
-    ["s11", "4"],
-    ["s12", "3"],
-    ["s13", "3"],
-    ["s14", "2"],
-    ["s15", "4"],
-    ["s16", "3"]
-]);
-
-const ABC_SELECT_GROUP_THREE_ANSWERS = new Map([
-    ["s21", "3"],
-    ["s22", "2"],
-    ["s23", "1"],
-    ["s24", "2"],
-    ["s25", "3"],
-    ["s26", "3"]
-]);
-
-const ABC_GROUP_ONE_CHART_ANSWERS = new Map([
-    ["x", "物料累计品目数百分比"],
-    ["y", "平均资金占用额累计百分比"]
-]);
-
-const ABC_GROUP_TWO_CHART_ANSWERS = new Map([
-    ["x1", "物料累计品目数百分比"],
-    ["y1", "累计出库频次百分比"]
-])
-
-const ABC_GROUP_THREE_CHART_ANSWERS = new Map([
-    ["x2", "物料累计品目数百分比"],
-    ["y2", "累计出库量百分比"]
-])
 /**
  * 此类用来计算学生在页面上操作的分数。
  * numSubmit - 提交次数
@@ -103,8 +61,10 @@ function checkSelectionResultAndRefresh(
     }
 
     // Update score.
-    baidieScoreKeeper.incNumSubmit();
-    baidieScoreKeeper.incNumDropDownCorrectOpBy(correctCount);
+    if (baidieScoreKeeper) {
+        baidieScoreKeeper.incNumSubmit();
+        baidieScoreKeeper.incNumDropDownCorrectOpBy(correctCount);
+    }
 }
 
 function getSelectedValue(elementId) {
