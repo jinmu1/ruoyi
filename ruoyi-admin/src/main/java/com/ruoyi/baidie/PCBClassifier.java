6f12d7948a72c72431e8fa1484e8d9a963e8536d
jinmu1 <1612429531@qq.com>
2024-02-20 18:16:15 +0800

    pcb中PCB类型计算逻辑 (#70)
    1.增加了计算逻辑
    2.增加了测试方法

--- ruoyi-admin/src/main/java/com/ruoyi/baidie/PCBClassifier.java
+++ ruoyi-admin/src/main/java/com/ruoyi/baidie/PCBClassifier.java
@@ -23,9 +23,11 @@ import java.util.stream.IntStream;
  * PCB 处理类：将PCB分析的各项统计指标进行转换
  */
 public class PCBClassifier {
+    private static DecimalFormat df = new DecimalFormat("#.##");
 
     /**
      * 统计出库物料的基本储运单位类型（P表示托盘单位、C表示件单位、B表示单品）
+     * 按照对应关系计算现有出库量适合的包装类型
      *
      * @param outboundBasicInfos 基本的出库数据
      * @return 返回一个带有每个物料的储运单位的计算数据
@@ -56,7 +58,6 @@ public class PCBClassifier {
                 ).get(0)
         );
         info.setProductBoxRelation(info.getCorrespondenceA() * info.getCorrespondenceB());
-        DecimalFormat df = new DecimalFormat("#.##");
         info.setOutDivision(
                 Double.parseDouble(
                         df.format(
@@ -64,21 +65,71 @@ public class PCBClassifier {
                         )
                 )
         );
-        info.setDemandPalletQuantity(Math.floor(info.getOutDivision() * 100/ 100) );
+        info.setDemandPalletQuantity(Math.floor(info.getOutDivision() * 100 / 100));
         info.setRemainder(info.getOutQuantityN() - info.getDemandPalletQuantity() * info.getProductBoxRelation());
         info.setDivisionByA(Double.parseDouble(
                 df.format(
-                        (info.getOutQuantityN() - info.getDemandPalletQuantity()*info.getProductBoxRelation()) / info.getCorrespondenceA()
+                        (info.getOutQuantityN() - info.getDemandPalletQuantity() * info.getProductBoxRelation()) / info.getCorrespondenceA()
                 ))
         );
-        info.setDemandBoxQuantity(Math.floor(info.getDivisionByA() * 100/ 100) );
+        info.setDemandBoxQuantity(Math.floor(info.getDivisionByA() * 100 / 100));
         info.setDemandProductQuantity(Double.parseDouble(
                 df.format(
-                        (info.getOutQuantityN() - info.getDemandPalletQuantity()*info.getProductBoxRelation()-info.getDemandBoxQuantity()*info.getCorrespondenceA())
+                        (info.getOutQuantityN() - info.getDemandPalletQuantity() * info.getProductBoxRelation() - info.getDemandBoxQuantity() * info.getCorrespondenceA())
                 ))
         );
         return info;
     }
 
+    /**
+     * 统计出库物料的基本储运单位类型（P表示托盘单位、C表示件单位、B表示单品）
+     *
+     * @param outboundBasicInfos 基本的出库数据
+     * @return 返回一个带有每个物料的储运单位的计算数据
+     */
+    public static List<MaterialOutboundTypeInfo> pickPackagingType(
+            List<OutboundBasicInfo> outboundBasicInfos) {
+        // step1：首先计算总销售金额
+        final List<MaterialOutboundTypeInfo> entryWithTotalValue =
+                outboundBasicInfos
+                        .stream()
+                        .map(PCBClassifier::calculatePackagingType)
+                        .collect(Collectors.toList());
+        return entryWithTotalValue;
+    }
 
+    private static MaterialOutboundTypeInfo calculatePackagingType(OutboundBasicInfo outboundBasicInfo) {
+        MaterialPackagingInfo packagingInfo = calculateTotalValue(outboundBasicInfo);
+
+        MaterialOutboundTypeInfo info = new MaterialOutboundTypeInfo();
+        info.setMaterialCode(outboundBasicInfo.getMaterialCode());
+        info.setPalletQuantity(packagingInfo.getDemandPalletQuantity());
+        info.setBoxQuantity(packagingInfo.getDemandBoxQuantity());
+        info.setProductQuantity(packagingInfo.getDemandProductQuantity());
+        double palletQuantity = info.getPalletQuantity();
+        double boxQuantity = info.getBoxQuantity();
+        double productQuantity = info.getProductQuantity();
+        if (palletQuantity > 0) {
+            if (boxQuantity > 0) {
+                if (productQuantity > 0) {
+                    info.setPackagingMode("P+C+B");
+                } else {
+                    info.setPackagingMode("P+C");
+                }
+            } else if (productQuantity > 0) {
+                info.setPackagingMode("P+B");
+            } else {
+                info.setPackagingMode("P");
+            }
+        } else if (boxQuantity > 0) {
+            if (productQuantity > 0) {
+                info.setPackagingMode("C+B");
+            } else {
+                info.setPackagingMode("C");
+            }
+        } else if (productQuantity > 0) {
+            info.setPackagingMode("B");
+        }
+        return info;
+    }
 }
